e729e3bd678d180e1e8e80fbc73d5e1a0010a64f
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-05-22 22:51:50 +0300

    Added the search engine to javascript

--- src/weather.js
+++ src/weather.js
@@ -22,15 +22,14 @@ function transformDate(timestamp) {
 }
 
 function showTemperature(response) {
-  console.log(response.data);
+  //console.log(response.data);
   let cityElement = document.querySelector("#city");
   let description = document.querySelector("#description");
   let temperatureElement = document.querySelector("#temperature");
   let humidity = document.querySelector("#humidity");
   let speedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#weather-icon")
-
+  let iconElement = document.querySelector("#weather-icon");
 
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -38,11 +37,26 @@ function showTemperature(response) {
   humidity.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = transformDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function searchEngine(city) {
+  let apiKey = "e9dbb073ecb679b0932ba8a75a3681c8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiKey = "e9dbb073ecb679b0932ba8a75a3681c8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 
-axios.get(apiUrl).then(showTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  searchEngine(cityInput.value);
+}
+
+searchEngine("Nairobi");
+
+let formInput = document.querySelector("#search-form");
+formInput.addEventListener("submit", handleSubmit);
